0e4cc947be8735a6c40ceda568bea9ce7e7ad778
ameerkhan54587 <104880005+ameerkhan54587@users.noreply.github.com>
2023-06-01 01:27:05 +0500

    Add files via upload

---
+++ js/kb-source.js
@@ -0,0 +1,68 @@
+function notify(type, message) {
+  (() => {
+    let n = document.createElement("div");
+    let id = Math.random().toString(36).substr(2, 10);
+    n.setAttribute("id", id);
+    n.classList.add("notification", type);
+    n.innerText = message;
+    document.getElementById("notification-area").appendChild(n);
+    setTimeout(() => {
+      var notifications = document.getElementById("notification-area").getElementsByClassName("notification");
+      for (let i = 0; i < notifications.length; i++) {
+        if (notifications[i].getAttribute("id") == id) {
+          notifications[i].remove();
+          break;
+        }
+      }
+    }, 1000);
+  })();
+}
+
+function copyToClipboard(text) {
+  var tempTextarea = document.createElement('textarea');
+  tempTextarea.style.position = 'fixed';
+  tempTextarea.style.opacity = 0;
+  tempTextarea.value = text;
+  document.body.appendChild(tempTextarea);
+
+  tempTextarea.select();
+  document.execCommand('copy');
+
+  document.body.removeChild(tempTextarea);
+
+  notify("success", "Copy Successfully");
+}
+
+var buttons = document.querySelectorAll('.kb-source');
+buttons.forEach(function(button) {
+  var copyIcon = document.createElement('i');
+  copyIcon.classList.add('fa', 'fa-copy');
+  button.appendChild(copyIcon);
+
+  button.addEventListener('click', function(event) {
+    event.stopPropagation();
+    var paragraph = this.querySelector('p.hidden');
+    var paragraphText = paragraph ? paragraph.innerHTML.trim().replace(/<br>/g, '\n') : '';
+    if (paragraphText) {
+      copyToClipboard(paragraphText);
+    }
+  });
+});
+
+// Disable pointer events on the notification container while animating
+document.addEventListener("animationstart", function(event) {
+  if (event.animationName === "showNotification") {
+    var notification = event.target;
+    notification.parentNode.style.pointerEvents = "none";
+  }
+});
+
+// Enable pointer events on the notification container after animation ends
+document.addEventListener("animationend", function(event) {
+  if (event.animationName === "showNotification") {
+    var notification = event.target;
+    setTimeout(() => {
+      notification.parentNode.style.pointerEvents = "auto";
+    }, 1000);
+  }
+});
---
+++ js/script.js
@@ -0,0 +1,31 @@
+// Check if the user is already logged in
+if (localStorage.getItem('loggedIn') === 'true') {
+    // Redirect the user to the welcome page
+    window.location.href = 'welcome.html';
+  }
+  
+  document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+  
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+  
+    // Check if the username and password match the predefined values
+    if (username === 'admin' && password === 'admin') {
+      // Set logged-in status
+      localStorage.setItem('loggedIn', 'true');
+  
+      // Redirect to the welcome page
+      window.location.href = 'welcome.html';
+    } else {
+      alert('Incorrect username or password. Please try again.');
+    }
+  });
+  
+  
+ 
+
+
+
+  
+  
