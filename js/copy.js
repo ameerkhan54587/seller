b87708ae4a94d6ba9c4973e7b86f92ff780a9478
ameerkhan54587 <104880005+ameerkhan54587@users.noreply.github.com>
2023-08-07 20:39:34 +0500

    Add files via upload

---
+++ js/copy.js
@@ -0,0 +1,75 @@
+function notify(type, message) {
+    (() => {
+      let n = document.createElement("div");
+      let id = Math.random().toString(36).substr(2, 10);
+      n.setAttribute("id", id);
+      n.classList.add("notification", type);
+      n.innerText = message;
+      document.getElementById("notification-area").appendChild(n);
+      setTimeout(() => {
+        var notifications = document.getElementById("notification-area").getElementsByClassName("notification");
+        for (let i = 0; i < notifications.length; i++) {
+          if (notifications[i].getAttribute("id") == id) {
+            notifications[i].remove();
+            break;
+          }
+        }
+      }, 1000);
+    })();
+  }
+  
+  function copyToClipboard(text) {
+    var tempTextarea = document.createElement('textarea');
+    tempTextarea.style.position = 'fixed';
+    tempTextarea.style.opacity = 0;
+  
+    // Replace line breaks with actual line breaks
+    var textWithLineBreaks = text.replace(/<br\s*[\/]?>/gi, '\n');
+  
+    tempTextarea.value = textWithLineBreaks;
+  
+    document.body.appendChild(tempTextarea);
+  
+    tempTextarea.select();
+    document.execCommand('copy');
+  
+    document.body.removeChild(tempTextarea);
+  
+    notify('success', 'Copy Successful');
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  var buttons = document.querySelectorAll('.kb-source');
+  buttons.forEach(function(button) {
+    var copyIcon = document.createElement('i');
+    copyIcon.classList.add('fa', 'fa-copy');
+    button.appendChild(copyIcon);
+  
+    button.addEventListener('click', function(event) {
+      event.stopPropagation();
+      var paragraph = this.querySelector('p.hidden');
+      var paragraphText = paragraph ? paragraph.innerHTML.trim().replace(/<br>/g, '\n') : '';
+      if (paragraphText) {
+        copyToClipboard(paragraphText);
+      }
+    });
+  });
+  
+  
+  
+  
+  
+  
